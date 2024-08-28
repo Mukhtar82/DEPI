442e9c88fc2404af38837ace6696d81133f9abab
Mohammed Mokhtar <138318635+Mukhtar82@users.noreply.github.com>
2024-08-28 17:07:53 +0300

    Add files via upload

---
+++ calc/main.py
@@ -0,0 +1,51 @@
+
+from calc import add, subtract, multiply, divide
+
+def calculator():
+    while True:
+        print("\nSelect operation:")
+        print("1. Add")
+        print("2. Subtract")
+        print("3. Multiply")
+        print("4. Divide")
+        print("5. Exit")
+        
+        choice = input("Enter choice(1/2/3/4/5): ")
+
+        if choice == '5':
+            print("Exiting the calculator. Goodbye!")
+            break
+
+        if choice in ['1', '2', '3', '4']:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+
+            if choice == '1':
+                print(f"{num1} + {num2} = {add(num1, num2)}")
+                x = input("\n Choose 1 if you want to make another operation ")
+                if x != '1':
+                    print("Exiting the calculator. Goodbye!")
+                    break
+
+            elif choice == '2':
+                print(f"{num1} - {num2} = {subtract(num1, num2)}")
+                x = input("\n Choose 1 if you want to make another operation ")
+                if x != '1':
+                    print("Exiting the calculator. Goodbye!")
+                    break
+            elif choice == '3':
+                print(f"{num1} * {num2} = {multiply(num1, num2)}")
+                x = input("\n Choose 1 if you want to make another operation ")
+                if x != '1':
+                    print("Exiting the calculator. Goodbye!")
+                    break
+            elif choice == '4':
+                print(f"{num1} / {num2} = {divide(num1, num2)}")
+                x = input("\n Choose 1 if you want to make another operation ")
+                if x != '1':
+                    print("Exiting the calculator. Goodbye!")
+                    break
+        else:
+            print("Invalid input")
+
+calculator()
